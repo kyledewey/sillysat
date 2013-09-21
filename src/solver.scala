b0ff0c5c869eb8d08a29377826dff11c5d25efa4
Kyle Dewey <kyledewey@cs.ucsb.edu>
2013-09-21 16:41:24 -0700

    Extremely basic learning solver implementation.

--- src/solver.scala
+++ src/solver.scala
@@ -3,6 +3,45 @@ package sillysat.solver
 import scala.annotation.tailrec
 import sillysat.syntax._
 
+case class SolverState(assignments: Map[Variable, Boolean], implGraph: Map[Variable, (Clause, Variable)]) {
+  assert(implGraph.keys.forall(k => assignments.contains(k)))
+
+  def makeChoice(variable: Variable, choice: Boolean): SolverState = {
+    assert(!assignments.contains(variable))
+    copy(assignments = assignments + (variable -> choice))
+  }
+
+  def addAssignment(becauseOf: Option[Variable], clause: Clause, changing: Literal): SolverState = {
+    assert(!assignments.contains(changing.v))
+    val newGraph = 
+      if (becauseOf.isEmpty || implGraph.contains(becauseOf.get)) {
+        implGraph
+      } else {
+        implGraph + (becauseOf.get -> (clause, changing.v))
+      }
+    
+    copy(assignments = assignments + (changing.v -> !changing.isNegated),
+         implGraph = newGraph)
+  }
+
+  def roots(): Set[Variable] =
+    implGraph.keySet -- implGraph.values.map(_._2)
+
+  // if there aren't any roots, then the clause must be unsat - this
+  // indicates that we made no arbitrary decisions
+  def makeConflictClause(): Option[Clause] = {
+    val rts = roots
+    if (rts.isEmpty) {
+      None
+    } else {
+      Some(
+        Clause(
+          rts.map(v =>
+            if (assignments(v)) NegativeAtom(v) else PositiveAtom(v)).toSeq))
+    }
+  }
+}
+
 object Solver {
   // determines if a given clause is satisfied based on assignments so far
   // if it's satisfied, then adding any assignments will not change this
@@ -32,84 +71,95 @@ object Solver {
   }
 
   // If the given clause is a unit clause, then it will
-  // make an assignment based on it.
-  def tryApplyUnit(clause: Clause, assignments: Map[Variable, Boolean]): Map[Variable, Boolean] = {
-    unitLiteral(clause, assignments) match {
-      case Some(lit) => {
-        assert(!assignments.contains(lit.v))
-        assignments + (lit.v -> !lit.isNegated)
-      }
-      case None => {
-        assignments
-      }
-    }
-  }
+  // make an assignment based on it.  Takes the variable which changed
+  // which forced the unit clause switch, and returns a tuple of the new
+  // solver state along with which variable was changed.  If it changes nothing,
+  // it returns None
+  def tryApplyUnit(clause: Clause, becauseOf: Option[Variable], state: SolverState): Option[(SolverState, Variable)] =
+    unitLiteral(clause, state.assignments).map(lit => 
+      (state.addAssignment(becauseOf, clause, lit), lit.v))
 
   def alwaysUnsatisfied(clause: Clause, assignments: Map[Variable, Boolean]): Boolean =
     clause.literals.forall(lit =>
       assignments.contains(lit.v) && assignments(lit.v) == lit.isNegated)
-
+  
+  // this means that both a positive atom and a negative atom with the
+  // same underlying variable is found
+  def literalsConflict(lits: List[Literal]): Boolean =
+    lits.foldLeft(Map[Variable, Set[Literal]]())((res, cur) =>
+      res + (cur.v -> (res.get(cur.v).getOrElse(Set()) + cur))).values.exists(_.size == 2)
+  
   // attempts to apply the unit clause rule to all clauses
-  // returns either a new series of assignments or None to indicate a conflict was found
-  def applyUnitToAll(clauses: List[Clause], assignments: Map[Variable, Boolean]): Option[Map[Variable, Boolean]] = {
-    // highly inefficient O(n^2) algorithm
+  // returns either a new solver state or a clause which explains a conflict
+  // repeats this process until a fixpoint is reached
+  def applyUnitToAll(clauses: List[Clause], becauseOf: Option[Variable], state: SolverState): Either[Option[Clause], SolverState] = {
     @tailrec
-    def recur(cls: List[Clause], asn: Map[Variable, Boolean]): Option[Map[Variable, Boolean]] = {
+    def recur(cls: List[Clause], becauseOf: Option[Variable], state: SolverState): Either[Option[Clause], SolverState] = {
       cls match {
         case head :: tail => {
-          if (alwaysUnsatisfied(head, asn)) {
-            None
+          if (alwaysUnsatisfied(head, state.assignments)) {
+            Left(state.makeConflictClause)
           } else {
-            val newAsn = tryApplyUnit(cls.head, asn)
-            if (newAsn ne asn) {
-              // assignment made - start again from beginning
-              recur(clauses, newAsn)
-            } else {
-              // assignment not made - continue processing
-              recur(tail, asn)
+            tryApplyUnit(cls.head, becauseOf, state) match {
+              case Some((newState, variableChanged)) =>
+                // assignment made - start again from the beginning
+                // TODO: This doesn't make the implication graph correctly,
+                // as it will skip over any other unit clauses that could
+                // still be applied without this assignment
+                recur(clauses, Some(variableChanged), newState)
+              case None =>
+                // assignment not made - continue processing
+                recur(tail, becauseOf, state)
             }
           }
         }
         case Nil =>
-          Some(asn)
+          Right(state)
       }
     }
 
-    recur(clauses, assignments)
+    recur(clauses, becauseOf, state)
   }
 
   def solve(problem: CNF): Option[Map[Variable, Boolean]] = {
-    val clauses = problem.clauses
+    var isUnsat = false
+    var clauses = problem.clauses
     val vars = problem.variables
-    def doSolve(asn: Map[Variable, Boolean]): Option[Map[Variable, Boolean]] = {
-      applyUnitToAll(clauses, asn) match {
-        case Some(newAsn) => {
-          val diff = vars -- newAsn.keys
+    def doSolve(becauseOf: Option[Variable], state: SolverState): Option[SolverState] = {
+      if (isUnsat) return None
+
+      applyUnitToAll(clauses, becauseOf, state) match {
+        case Right(newState) => {
+          val diff = vars -- newState.assignments.keys
           if (diff.isEmpty) {
             // we have an assignment for all variables
-            Some(newAsn)
+            Some(newState)
           } else {
             // some variables are unspoken for - choose one and set a value
             val choice = diff.head
-            doSolve(asn + (choice -> true)) match {
+            doSolve(Some(choice), newState.makeChoice(choice, true)) match {
               case s@Some(_) => s
-              case None => doSolve(asn + (choice -> false))
+              case None => doSolve(Some(choice), newState.makeChoice(choice, false))
             }
           }
         }
-        case None =>
+        case Left(Some(c)) => {
+          clauses ::= c
+          println("ADDING IMPLIED CLAUSE: " + c)
           None
+        }
+        case Left(None) => {
+          isUnsat = true
+          None
+        }
       }
     }
     
-    doSolve(Map()) match {
-      case s@Some(soln) => {
-        assert(soln.keySet == vars)
-        assert(clauses.forall(c => isSatisfied(c, soln)))
-        s
-      }
-      case None => None
-    }
+    doSolve(None, SolverState(Map(), Map())).map(soln => {
+      assert(soln.assignments.keySet == vars)
+      assert(clauses.forall(c => isSatisfied(c, soln.assignments)))
+      soln.assignments
+    })
   }
 }
 
